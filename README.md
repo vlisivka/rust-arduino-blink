3494aa678bbba9cf0cac3ff61aabd0e3d95531df
Volodymyr M. Lisivka <vlisivka@gmail.com>
2021-02-13 11:40:27 +0200

    Comments are added.

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Blink example for Arduino Uno in Rust. Developed on Fedora Linux 33.
 
 Requires rust nightly-2021-01-07, avr-gcc, avr-libc, and avrdude.
 
-ROM size: 304 bytes.
+ROM size: 304 bytes with "s" optimization level.
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
+// Standard library is not available.
 #![no_std]
+
+// No typical main function, with _start(), constructors, destructors, etc.
 #![no_main]
 
+// Use empty functions for panic handler.
+// Panic handler can be implemented, e.g. to blink led or print message
+// to serial, but I don't want it there.
 use avr_std_stub as _;
+
+// AVR HAL library. Crate is named "arduino_uno" in Cargo.toml.
 use arduino_uno::prelude::*;
 
+// Main function
 #[arduino_uno::entry]
 fn main() -> ! {
     let peripherals = arduino_uno::Peripherals::take().unwrap();
@@ -14,6 +23,7 @@ fn main() -> ! {
         peripherals.PORTD,
     );
 
+    // Pin D13 is connected to L led
     let mut led = pins.d13.into_output(&mut pins.ddr);
 
     loop {
